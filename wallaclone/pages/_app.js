a13d06b55dc9ae402e43eb6984ec46a029d82071
lmhm <lmhm.yeah@gmail.com>
2021-07-05 04:49:06 +0200

    login embellecido

--- wallaclone/pages/_app.js
+++ wallaclone/pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/login.css'
+import '@fontsource/roboto';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- wallaclone/pages/login.js
+++ wallaclone/pages/login.js
@@ -1,27 +1,57 @@
-
 import React from 'react'
 import Link from 'next/link';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import LockIcon from '@material-ui/icons/Lock';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
 
 const Login = () => {
+    const classes = useStyles();
     return (
-        <div className="main-container">
-            <h1>Página de Login</h1>
-            <Link href='/'> Go back home
-            
-             </Link >
-            <style jsx>{`
-            
-            h1{
-                color:red;
-            }
+        <div className="login-container">
+            <h1>Login</h1>
 
-            .main-container{
-                text-align:center;
-                margin: 5rem;
-            }
+            <FormControl>
+                <div className={classes.margin, "login-input"}>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <AccountCircle />
+                        </Grid>
+                        <Grid item>
+                            <TextField id="input-with-icon-grid" label="Username" />
+                        </Grid>
+                    </Grid>
+                </div>
+                <div className={classes.margin, "login-input"}>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <LockIcon />
+                        </Grid>
+                        <Grid item>
+                            <TextField id="input-with-icon-grid" label="Password" type="password" />
+                        </Grid>
+                    </Grid>
+                </div>
 
-            `}</style>
+                <Button size="large" className={classes.margin} variant="contained" color="primary" type="submit">
+                    Login
+                </Button>
+            </FormControl>
+
+            <Link href='/'>
+                Go back home
+            </Link >
         </div>
+
     )
 }
 
