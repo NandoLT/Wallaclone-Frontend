cda6a690fc0239bcb05a9e3f42352a98017e153e
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-07-07 12:18:22 +0200

    Intento de controlar los inputs del formulario y setear el estado con las credentials

--- wallaclone/pages/login.js
+++ wallaclone/pages/login.js
@@ -7,6 +7,27 @@ import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import LockIcon from '@material-ui/icons/Lock';
 import Button from '@material-ui/core/Button';
+import { useState } from 'react';
+
+
+const [credentials, setCredentials] = React.useState({
+    email:'',
+    password:''
+})
+
+const handleInputChange = event =>{
+    setCredentials(oldCredentials => {
+
+        const newCredentials = {
+            ...oldCredentials,
+            [event.target.name]: event.target.value,
+        }
+       return newCredentials
+    });
+    
+}
+
+
 
 const useStyles = makeStyles((theme) => ({
     margin: {
@@ -14,20 +35,31 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+
+
+// const handleSubmit = (remember, credentials) =>{
+//     dispatch(authLogin(credentials))
+
+// }
+
+const handleSubmit = () =>{
+    console.log('Has hecho submit en el formulario')
+}
+
 const Login = () => {
     const classes = useStyles();
     return (
         <div className="login-container">
             <h1>Login</h1>
 
-            <FormControl>
+            <FormControl onSubmit={handleSubmit}>
                 <div className={classes.margin, "login-input"}>
                     <Grid container spacing={1} alignItems="flex-end">
                         <Grid item>
                             <AccountCircle />
                         </Grid>
                         <Grid item>
-                            <TextField id="input-with-icon-grid" label="Username" />
+                            <TextField onChange ={handleInputChange} name="email" id="input-with-icon-grid" label="Username" />
                         </Grid>
                     </Grid>
                 </div>
@@ -37,7 +69,7 @@ const Login = () => {
                             <LockIcon />
                         </Grid>
                         <Grid item>
-                            <TextField id="input-with-icon-grid" label="Password" type="password" />
+                            <TextField onChange ={handleInputChange} name="password" id="input-with-icon-grid" label="Password" type="password" />
                         </Grid>
                     </Grid>
                 </div>
