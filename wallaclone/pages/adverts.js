c1b8424458ddee6839bd3851d9880799a04036b7
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-07-08 16:38:31 +0200

    Conectada la página de anuncios al estado de redux y renderización dinamica en funcion de si el usuario está loggeado o no

--- wallaclone/pages/adverts.js
+++ wallaclone/pages/adverts.js
@@ -1,14 +1,65 @@
 
 import React from 'react'
 import Link from 'next/link';
+import {connect} from 'react-redux';
+import { getIsLogged } from '../store/selectors';
 
-const Adverts = () => {
+const fakeAdverts = [
+    {
+        name: "Anuncio 1",
+        price: 45.67,
+        onSale: true,
+    },
+    {
+        name: "Anuncio 2",
+        price: 85.63,
+        onSale: false,
+    },
+    {
+        name: "Anuncio 3",
+        price: 4.17,
+        onSale: true,
+    },
+]
+
+const Adverts = ({isLogged}) => {
+
+
     return (
         <div className="main-container">
             <h1>Página de Anuncios</h1>
             <Link href='/'> Go back home
             
              </Link >
+             
+            {isLogged 
+            ? 
+            <section className="ads-container">
+                
+                {fakeAdverts.map(advert => {
+                const {name, price, onSale} = advert;
+                return (
+                        
+                             <div className="ad-wrapper">
+                            <h3>{name}</h3>
+                            <p>Precio: {price} €</p>
+                            {advert ? <p>En venta</p> : <p>Compro</p>}
+                            </div>
+
+                        
+                       
+                    )
+                })}
+            
+
+            </section>
+            :
+            <h2> Usuario no loggeado</h2>
+                 }
+
+          
+          
+
             <style jsx>{`
             
             h1{
@@ -20,9 +71,37 @@ const Adverts = () => {
                 margin: 5rem;
             }
 
+            .ad-wrapper{
+                max-width: 200px;
+                min-width: 200px;
+                max-height: 200px;
+                min-height: 200px;
+                padding-top: 20px;
+                padding-bottom: 20px;
+                padding-left: 30px;
+                padding-right:30px;
+                margin:10px;
+                border-width: 1px;
+                border-radius: 5px;
+                border-style: solid;
+                border-color: lightslategray;
+            }
+
+            .ads-container{
+                margin: 50px;
+                margin-top:30px;
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                
+            }
+
             `}</style>
+
+
         </div>
     )
 }
+const mapStateToProps = state => ({isLogged: getIsLogged(state)})
 
-export default Adverts
+export default connect(mapStateToProps)(Adverts)
