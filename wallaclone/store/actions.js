591641376dbdad39233515c23c89321d7d900b2a
lmhm <lmhm.yeah@gmail.com>
2021-07-09 03:08:50 +0200

    Add redux and create template action

--- wallaclone/store/actions.js
+++ wallaclone/store/actions.js
@@ -1,20 +1,32 @@
 import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_REGISTER } from "./types";
 
 
-export const  authRegister = () =>{
+export const authRegister = () => {
     return {
         type: AUTH_REGISTER,
     }
 }
 
-export const  authLogin = () =>{
+export const authLogin = () => {
     return {
         type: AUTH_LOGIN,
     }
 }
 
-export const  authLogout = () =>{
+export const authLogout = () => {
     return {
         type: AUTH_LOGOUT,
+    }
+}
+
+
+export const authLoginAction = (remmeber, ...credentials) => {
+    return async function (dispatch, getState, { api }) {
+        try {
+            //Manejo de Api
+            dispatch(authLogin())
+        } catch (err) {
+
+        }
     }
 }
--- wallaclone/store/index.js
+++ wallaclone/store/index.js
@@ -1,12 +1,12 @@
-
-
-import {createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import * as api from '../api'
 
 
-const configureStore = ({ preloadedState }) => {
-    const store = createStore(reducer, preloadedState, composeWithDevTools());
+const configureStore = ({ preloadedState, api }) => {
+    const store = createStore(reducer, preloadedState, composeWithDevTools(applyMiddleware(thunk.withExtraArgument({ api }))));
     return store
 }
 
