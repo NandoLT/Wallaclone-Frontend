7bad0c4f376f9a7f469d63b15cdb9474061602c8
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-07-16 18:39:44 +0200

    Reset error when clicking alert box

--- wallaclone/components/Alert.js
+++ wallaclone/components/Alert.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Alert from '@material-ui/lab/Alert';
-import {connect} from 'react-redux';
+import {connect, useDispatch} from 'react-redux';
 import { getError } from '../store/selectors';
+import { authResetState } from '../store/actions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,11 +15,17 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function SimpleAlerts({error}) {
+
+const dispatch = useDispatch();
+
   const classes = useStyles();
+  const resetError = () => {
+    dispatch(authResetState());
+  }
 
   return (
-    <div className={classes.root}>
-      <Alert severity="error">{error}</Alert>
+    <div onClick={resetError} className={classes.root}>
+      <Alert  severity="error">{error}</Alert>
      
     </div>
   );
