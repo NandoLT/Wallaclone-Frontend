961e8c3bdd1d7c979a2c1c718d5353daf8a04189
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-07-18 19:23:51 +0200

    Implementada validación en formulario de login

--- wallaclone/pages/login.js
+++ wallaclone/pages/login.js
@@ -73,6 +73,19 @@ const Login = ({ isLogged, isLoading, error }) => {
         setRemember(!remember);
     }
 
+    const validation = () => {
+
+        if (!credentials.email) {
+            return true
+        };
+
+        if(!credentials.password){
+            return true
+        }
+
+        return false;
+    }
+
     return (
         <div className="login-container">
             <h1>Login</h1>
@@ -87,7 +100,7 @@ const Login = ({ isLogged, isLoading, error }) => {
                             <AccountCircle />
                         </Grid>
                         <Grid item>
-                            <TextField onChange={event => handleInputChange(event)} name="email" id="input-with-icon-grid" label="Username" value={credentials.email} />
+                            <TextField required onChange={event => handleInputChange(event)} name="email" id="input-with-icon-grid" label="Username" value={credentials.email} />
                         </Grid>
                     </Grid>
                 </div>
@@ -97,7 +110,7 @@ const Login = ({ isLogged, isLoading, error }) => {
                             <LockIcon />
                         </Grid>
                         <Grid item>
-                            <TextField onChange={handleInputChange} name="password" id="input-with-icon-grid" label="Password" type="password" value={credentials.password} />
+                            <TextField required onChange={handleInputChange} name="password" id="input-with-icon-grid" label="Password" type="password" value={credentials.password} />
                         </Grid>
                     </Grid>
                 </div>
@@ -112,7 +125,7 @@ const Login = ({ isLogged, isLoading, error }) => {
                     />
                 </div>
 
-                {!isLoading && <Button size="large" className={classes.margin} variant="contained" color="primary" type="submit">
+                {!isLoading && <Button disabled={validation()} size="large" className={classes.margin} variant="contained" color="primary" type="submit">
                     Login
                 </Button>}
 
