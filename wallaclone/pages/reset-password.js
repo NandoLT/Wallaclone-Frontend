f08c09efd372dd689c93bf33e335b0663da14db8
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-07-29 14:27:55 +0200

    Creada estructura de resetPassword, falta llamada a la API

---
+++ wallaclone/pages/reset-password.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { useDispatch, connect } from 'react-redux';
+import Loading from '../components/Loading';
+import Alert from '../components/Alert'
+import {getIsLoading, getError, getUserId} from '../store/selectors'
+import styles from '../styles/Home.module.css'
+import EmailIcon from '@material-ui/icons/Email';
+import LockIcon from '@material-ui/icons/Lock';
+
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
+
+const ResetPassword = ({isLoading, error}) => {
+
+    const classes = useStyles();
+    const dispatch= useDispatch();
+    
+
+    
+
+    const [passwords, setPasswords] = React.useState({
+        newPassword:'',
+        confirmNewPassword:'',
+
+    });
+    
+
+    const handleInputChange = event => {
+        setPasswords(oldPasswords => {
+
+            const newPasswords = {
+                ...oldPasswords,
+                [event.target.name]: event.target.value,
+            }
+            return newPasswords
+        });
+
+    }
+
+
+    const validation = () => {
+
+        if (!passwords.newPassword) {
+            return true
+        };
+
+        if (!passwords.confirmNewPassword) {
+            return true
+        };
+
+        if(passwords.newPassword !== passwords.confirmNewPassword){
+            return true
+        }
+
+
+        return false;
+    }
+
+    const handleSubmit = (event) => {
+        
+        event.preventDefault();
+        console.log(passwords)
+        //dispatch(authrecoverPasswordAction(email));
+
+    }
+
+    return (
+        <div className="login-container">
+            <h1>Establece tu nueva contraseña</h1>
+            <p>Por favor, elige tu nueva contraseña y ponla en los cuadros más abajo </p>
+
+        <form onSubmit={handleSubmit} className="login-form">
+        <div className={classes.margin, "login-input"}>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <LockIcon />
+                        </Grid>
+                        <Grid item>
+                            <TextField required onChange={handleInputChange} name="newPassword" id="input-with-icon-grid" label="Nueva contraseña" type="password" value={passwords.newPassword} />
+                        </Grid>
+                    </Grid>
+        </div>
+        <div className={classes.margin, "login-input"}>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <LockIcon />
+                        </Grid>
+                        <Grid item>
+                            <TextField required onChange={handleInputChange} name="confirmNewPassword" id="input-with-icon-grid" label="Confirma la nueva contraseña" type="password" value={passwords.confirmNewPassword} />
+                        </Grid>
+                    </Grid>
+        </div>
+               
+                
+
+                {!isLoading && <Button disabled={validation()} size="large" className={classes.margin} variant="contained" color="primary" type="submit">
+                    Recuperar contraseña
+                </Button>}
+
+                {error && <Alert />}
+
+
+            </form>
+            {isLoading && <Loading />}
+
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    isLoading: getIsLoading(state),
+    error: getError(state),
+    
+}); 
+
+export default connect(mapStateToProps)(ResetPassword)
