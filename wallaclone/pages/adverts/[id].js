bcb41dd6599ccf732328765148ba71584c3c946b
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-08-24 10:47:43 +0200

    Comienzo aproximacion a desarrollo detalle del anuncio

--- wallaclone/pages/adverts/[id].js
+++ wallaclone/pages/adverts/[id].js
@@ -84,9 +84,16 @@ const adBelongstoUser = () => {
 
     return (
         <div className="register-container">
+            <h1>HOLA</h1>
             {advert ?
+
+                
+
+                // EMPIEZA AQUÍ ABAJO EL CÓDIGO ORIGINAL
+
+                
                 <div >
-                    <img src={advert.photo ?  `https://pruebas-wallaclone.s3.eu-west-3.amazonaws.com/${advert.userId}/${advert.photo[0]}` : '/img/image-not-available.png'} />
+                    <img className="advert-photo" src={advert.photo ?  `https://pruebas-wallaclone.s3.eu-west-3.amazonaws.com/${advert.userId}/${advert.photo[0]}` : '/img/image-not-available.png'} />
                     {editMode && <div className= "pointer" onClick={() => console.log("Borrar Imagen")}> <DeleteForeverIcon onClick={()=> console.log("Borrar imagen")} color="secondary" fontsize="large"/> Borrar Imagen</div> }
                     {editMode && <div><Button  onClick={handleEditMode} disabled={false} size="large" className={classes.margin} variant="contained" color="secondary" type="submit">
                     Deshacer cambios
@@ -169,9 +176,18 @@ const adBelongstoUser = () => {
                 <div></div>
             }
             
-         
+            <style jsx>{`
+                    
+                    .advert-photo{
+                        width: 500px;
+                        height: 300px;
+                    }
+
+                    `}</style>
 
         </div>
+        
+        
     );
 }
 
