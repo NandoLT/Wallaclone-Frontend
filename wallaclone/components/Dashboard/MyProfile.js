f6763321b3075dad958bc7fd3e7ffa3690b4ff43
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-08-26 10:38:48 +0200

    Conditional rendering de la descripcion y la provincia

--- wallaclone/components/Dashboard/MyProfile.js
+++ wallaclone/components/Dashboard/MyProfile.js
@@ -10,6 +10,26 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const MyProfile = () => {
+
+    const[profileDetails, setProfileDetails] = useState({
+        photo:null,
+        description:null,
+        province: null,
+    })
+    
+    const [photoUploaded, setPhotoUploaded] = React.useState(false);
+
+    const setPhoto = event => {
+        setPhotoUploaded(true);
+      setProfileDetails(oldProfileDetails => {
+          const newProfileDetails = {
+              ...oldProfileDetails,
+              'photo': event.target.files[0]
+          }
+          return newProfileDetails;
+      });
+    }
+
     return (
         <div>
             
@@ -17,20 +37,45 @@ const MyProfile = () => {
                 
 
                 <label for ="avatar"> <div className="image-container" ><Image  src="/photo-upload.png" alt="me" width="100%" height="100%" /> </div>  </label>
-                    <input hidden type="file"
-            id="avatar" name="avatar"
-            accept="image/png, image/jpeg"/>
+                    <input 
+                    hidden type="file"
+                    id="avatar" 
+                    name="avatar"
+                    accept="image/png, image/jpeg"
+                    onChange={setPhoto}
+                    />
+
+
 
                 
             
                 <h1>John Doe</h1>
-                <p class="title">CEO & Founder, Example</p>
-                <p>Barcelona</p>
-                <a href="#"><i class="fa fa-dribbble"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-linkedin"></i></a>
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <p><button>5 anuncios subidos</button></p>
+                {profileDetails.description ? <h4 className="title">{profileDetails.description}</h4> : <h4> Todavía no has añadido una descripción a tu perfil. Hac click en editar para contarnos sobre tí</h4> }
+
+                {profileDetails.province ? <h5>{profileDetails.province}</h5> : <h5>Aún no has especificado tu provincia. Hac click en editar para añadirla a tu perfil </h5>}
+               
+                <div className="icons-container">
+                    <div className="icon-container">
+                        <div className="icon" ><Image className="icon" src="/star.png" alt="me" width="30" height="30" />
+                        <div className="icon-title">Mis anuncios</div>
+                    </div>
+                </div>
+
+                <div className="icon-container">
+                    <div className="icon" ><Image className="icon" src="/heart.png" alt="me" width="30" height="30" />
+                    <div className="icon-title">Mis favoritos</div>
+                    </div>
+                </div>
+                
+                <div className="icon-container">
+                    <div className="icon" ><Image className="icon" src="/email (1).png" alt="me" width="30" height="30" />
+                    <div className="icon-title">Mis conversaciones</div>
+                    </div>
+
+                    </div>
+            
+                 </div>
+                <p><button>Editar perfil</button></p>
                 
              </div>
 
@@ -45,6 +90,23 @@ const MyProfile = () => {
 
                     .image-container {
                         cursor: pointer;
+                    }
+
+                    .icon-container {
+                        margin: 0 35px;
+                    }
+
+                    .icon-title {
+                        font-size: 15px;
+                    }
+
+                    .icons-container {
+                        padding: 15px 0;
+                        display: flex;
+                        flex-direction:row;
+                        justify-content: center;
+                        
+                        border-top: 1px solid black;
                     }
                     
                     .title {
